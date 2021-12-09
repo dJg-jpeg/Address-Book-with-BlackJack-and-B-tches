1ac8d67d9edd8837c85af64ac6ab64a8cfae7531
Nazar <oleksiievets.n.o@gmail.com>
2021-12-09 19:09:45 +0100

    Update bot_classes.py

--- bot_classes.py
+++ bot_classes.py
@@ -1,6 +1,6 @@
 from collections import UserDict
 from datetime import datetime
-from typing import List
+from typing import Optional, List
 import re
 
 
@@ -74,6 +74,40 @@ class Phone(Field):
         self.__value = new_phone
 
 
+class Tag(Field):
+    """Tag for notes of the contact"""
+
+    def __str__(self):
+        return f"{self.value}"
+
+
+class Note(Field):
+    """Notes of the contact"""
+
+    def __init__(self, value, tag: List[Tag] = None):
+        if tag is None:
+            self.tag = []
+        else:
+            self.tag = [Tag(p) for p in tag]
+        super(Note, self).__init__(value)
+
+    def __str__(self):
+        return f"The note is '{self.value}'. And the tags are {[p.value for p in self.tag]}"
+
+    @property
+    def value(self) -> str:
+        return self.__value
+
+    @value.setter
+    def value(self, value: str) -> None:
+        self.__value = value
+
+    def add_tag(self, input_tag: str) -> None:
+        tag = Tag(input_tag)
+        if tag not in self.tag:
+            self.tag.append(tag)
+
+
 class Address(Field):
     """Address of the contact"""
 
@@ -92,18 +126,6 @@ class Email(Field):
         self.__value = new_email
 
 
-class AddressBook(UserDict):
-    """All contacts data"""
-
-    def add_record(self, record: dict) -> None:
-        new_record = Record(name=record['name'],
-                            phones=record['numbers'],
-                            birthday=record['birthday'],
-                            addresses=record['addresses'],
-                            email=record['email'])
-        self.data[new_record.name.value] = new_record
-
-
 class Record:
     """Records(contacts) in users contact book.
     Only one name , birthday and email, but it can be more than one phone and more than one address"""
@@ -112,7 +134,8 @@ class Record:
                  phones: List[str] = None,
                  birthday: str = None,
                  addresses: List[str] = None,
-                 email: str = None) -> None:
+                 email: str = None,
+                 note: List[str] = None) -> None:
         self.phone = []
         if phones is not None:
             for p in phones:
@@ -137,3 +160,45 @@ class Record:
             self.email.value = email
         else:
             self.email = None
+        if note is None:
+            self.note = []
+        else:
+            self.note = [Note(p) for p in note]
+
+    def add_note(self, input_note: str, input_tag: List[str] = None) -> None:
+        note_to_add = Note(input_note, input_tag)
+        self.note.append(note_to_add)
+        return [p.value for p in self.note]
+
+    def find_note(self, note: str) -> Optional[Note]:
+        for p in self.note:
+            if p.value == note:
+                return p
+
+    def delete_note(self, note: str) -> None:
+        note_to_delete = self.find_note(note)
+        self.note.remove(note_to_delete) if note_to_delete else None
+
+    def __str__(self):
+        return f"Record of {self.name.value} has phones {[p.value for p in self.phone]}. And following notes: {[p.value for p in self.note]}"
+
+
+class AddressBook(UserDict):
+    """All contacts data"""
+
+    def add_record(self, record: dict) -> None:
+        new_record = Record(name=record['name'],
+                            phones=record['numbers'],
+                            birthday=record['birthday'],
+                            addresses=record['addresses'],
+                            email=record['email'])
+        self.data[new_record.name.value] = new_record
+
+    def search_for_notes(self, search_symbols: str) -> Optional[Record]:
+        contacts = list(self.data.values())
+        found_contacts = set()
+        for contact in contacts:
+            contact_notes = ' '.join([p.value for p in contact.note])
+            if search_symbols in contact_notes:
+                found_contacts.add(contact)
+        return found_contacts
